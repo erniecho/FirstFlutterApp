7d7fa06b6d38a1879003bd7633be26a260371ceb
erniecho <erniecho@gmail.com>
2019-03-06 16:10:56 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart'; //add this line to import the english package
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final WordPair wordPair = new WordPair.random(); //this picks a random word value.
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: new Center( //change const to new object.
+          //child: const Text('Hello World'),//remove hello word.
+          //child: new Text(wordPair.asPascalCase), //Generate a text
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> { //What does line State<RandomWords> do?
+  //@override //what does override do in dart?
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget build(BuildContext context) {
+    //final WordPair wordPair = new WordPair.random();  //what does final or WordPair do?
+    //return new Text(wordPair.asPascalCase);
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return new Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+RandomWordsState createState() => new RandomWordsState();
+}
